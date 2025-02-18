a0cfa8c1a99d455b3686d56afaabf3959594081a
Jghoul229 <103498626+Jghoul229@users.noreply.github.com>
2025-02-18 15:20:22 +0300

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+function downloadSkin() {
+  alert("Скин загружается...");
+  // Здесь можно добавить функционал для скачивания скинов
+}
+
+function downloadMap() {
+  alert("Карта загружается...");
+  // Здесь можно добавить функционал для скачивания карт
+}
+
+function playPreview() {
+  const video = document.getElementById('preview-video');
+  video.play();
+  setTimeout(() => {
+    video.pause();
+    video.currentTime = 0;
+  }, 5000); // Останавливаем видео через 5 секунд
+}
 
